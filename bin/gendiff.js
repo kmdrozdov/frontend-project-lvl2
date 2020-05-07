99008c71b76de4b716d6e74c1156b44b7422b194
Kirill Drozdov <kirill.m.drozdov@gmail.com>
2020-05-07 23:16:38 +0300

    Выполнен второй шаг

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ const { version } = require('../package.json');
 
 program
   .version(version)
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
