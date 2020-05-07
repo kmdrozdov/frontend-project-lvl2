3135bba6e29b43663266f33ba1be7a97005fc8f2
Kirill Drozdov <kirill.m.drozdov@gmail.com>
2020-05-07 22:58:24 +0300

    Выполнен первый шаг

---
+++ bin/gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+const { version } = require('../package.json');
+
+program
+  .version(version)
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
