440cf11baa2f1267c32e19e41d2a891392a85494
Kirill Drozdov <kirill.m.drozdov@gmail.com>
2020-06-26 00:13:17 +0300

    Переделано формирование тест-кейсов

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,26 +5,27 @@ import genDiff from '../src/index.js';
 
 const getFixturesPath = (filename) => join('__fixtures__', filename);
 const readFile = (fileName) => readFileSync(getFixturesPath(fileName), 'utf-8');
-const cartesian = (...arrays) => arrays.reduce(
-  (acc, currElem) => acc.flatMap((x) => currElem.map((y) => x.concat([y]))),
-  [[]],
-);
 
-const cases = cartesian(
-  ['stylish', 'plain', 'json'],
-  ['before.json', 'before.yml', 'before.ini'],
-  ['after.json', 'after.yml', 'after.ini'],
+const firstFileNames = ['before.json', 'before.yml', 'before.ini'];
+const secondFileNames = ['after.json', 'after.yml', 'after.ini'];
+
+const stylishContent = readFile('stylish_result.txt');
+const plainContent = readFile('plain_result.txt');
+const jsonContent = readFile('json_result.txt');
+const cases = firstFileNames.flatMap(
+  (firstFileName) => secondFileNames.map((secondFileName) => [firstFileName, secondFileName]),
 );
 
 describe('Main cases', () => {
   test.each(cases)(
-    'Case for "%s" format with "%s" and "%s" files',
-    (format, firstFileName, secondFileName) => {
+    'Case for "%s" and "%s" files',
+    (firstFileName, secondFileName) => {
       const firstFilePath = getFixturesPath(firstFileName);
       const secondFilePath = getFixturesPath(secondFileName);
-      const resultContent = readFile(`${format}_result.txt`);
 
-      expect(genDiff(firstFilePath, secondFilePath, format)).toEqual(resultContent);
+      expect(genDiff(firstFilePath, secondFilePath, 'stylish')).toEqual(stylishContent);
+      expect(genDiff(firstFilePath, secondFilePath, 'plain')).toEqual(plainContent);
+      expect(genDiff(firstFilePath, secondFilePath, 'json')).toEqual(jsonContent);
     },
   );
 });
